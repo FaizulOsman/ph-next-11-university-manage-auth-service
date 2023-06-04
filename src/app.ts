2ed475d951d77aba4a94f654af168cf6236c9ab2
FaizulOsman <faizulosmanrabby@gmail.com>
2023-06-04 23:16:39 +0600

    error setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import cors from 'cors'
 import usersRouter from './app/modules/users/users.route'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 const app: Application = express()
 
 app.use(cors())
@@ -12,8 +13,12 @@ app.use(express.urlencoded({ extended: true }))
 // Application Routes
 app.use('/api/v1/users/', usersRouter)
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world')
-})
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   // throw new ApiError(400, 'Orre Baba Error')
+//   // next('Orre Baba Error')
+// })
+
+// Global Error Handler
+app.use(globalErrorHandler)
 
 export default app
