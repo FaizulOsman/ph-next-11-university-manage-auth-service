849d4fc5a475411de9f5da4de1046cb591b81095
FaizulOsman <faizulosmanrabby@gmail.com>
2023-06-06 16:53:51 +0600

    important comment added

--- src/server.ts
+++ src/server.ts
@@ -14,30 +14,37 @@ let server: Server
 
 async function bootstrap() {
   try {
+    // Connect to the database
     await mongoose.connect(config.database_url as string)
     logger.info('🔥 Database connected 🔥')
 
+    // Start the server
     server = app.listen(config.port, () => {
       logger.info(`The app is running on port: ${config.port}`)
     })
   } catch (error) {
+    // Log error if database connection fails
     errorLogger.error('Failed to connect database ', error)
   }
 
+  // Gracefully shutting down the server in case of unhandled rejection
   process.on('unhandledRejection', error => {
     if (server) {
+      // Close the server and log the error
       server.close(() => {
         errorLogger.error(error)
         process.exit(1)
       })
     } else {
+      // If server is not available, exit the process
       process.exit(1)
     }
   })
 }
+// Call the bootstrap function to start the application
 bootstrap()
 
-// SIGTERM Error Handler
+// Handling SIGTERM signal
 process.on('SIGTERM', () => {
   logger.info('SIGTERM is received')
   if (server) {
