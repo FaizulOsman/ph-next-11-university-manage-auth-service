d1068718f462265fc17ba80f23d27b2a07f7361a
FaizulOsman <faizulosmanrabby@gmail.com>
2023-06-06 23:51:25 +0600

    13-10 feat: academicSemester all setup done

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,14 +1,14 @@
 import express from 'express';
-// import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
 import validateRequest from '../../middlewares/validateRequest';
 const router = express.Router();
 
 // Routes
 router.post(
-  '/create-academic-semester',
-  validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema)
-  //   AcademicSemesterController.createAcademicSemester
+  '/create-semester',
+  validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  AcademicSemesterController.createSemester
 );
 
 export const AcademicSemesterRoutes = router;
