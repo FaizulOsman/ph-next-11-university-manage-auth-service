18d486f4644a939cb083d09d4287f4a09b2ff2ee
FaizulOsman <faizulosmanrabby@gmail.com>
2023-06-14 13:29:59 +0600

    fix: update bug fix

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -9,11 +9,14 @@ import { SortOrder } from 'mongoose';
 import ApiError from '../../../errors/ApiError';
 import httpStatus from 'http-status';
 
+/* Get All Students */
 const getAllStudents = async (
   filters: IStudentFilters,
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IStudent[]>> => {
   const { searchTerm, ...filtersData } = filters;
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
 
   const andConditions = [];
 
@@ -36,23 +39,20 @@ const getAllStudents = async (
     });
   }
 
-  const { page, skip, limit, sortBy, sortOrder } =
-    paginationHelpers.calculatePagination(paginationOptions);
+  const sortConditions: { [key: string]: SortOrder } = {};
 
-  const sortCondition: { [key: string]: SortOrder } = {};
-
-  if (sortBy || sortOrder) {
-    sortCondition[sortBy] = sortOrder;
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
   }
 
   const whereConditions =
-    andConditions?.length > 0 ? { $and: andConditions } : {};
+    andConditions.length > 0 ? { $and: andConditions } : {};
 
   const result = await Student.find(whereConditions)
     .populate('academicSemester')
     .populate('academicDepartment')
     .populate('academicFaculty')
-    .sort(sortCondition)
+    .sort(sortConditions)
     .skip(skip)
     .limit(limit);
 
@@ -83,44 +83,41 @@ const updateStudent = async (
   // Throw error if student not exist
   const isExist = await Student.findOne({ id });
   if (!isExist) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Student not found');
+    throw new ApiError(httpStatus.NOT_FOUND, 'Student not found !');
   }
 
   const { name, guardian, localGuardian, ...studentData } = payload;
-
   const updatedStudentData: Partial<IStudent> = { ...studentData };
 
   // Dynamically handle name update
   if (name && Object.keys(name).length > 0) {
     Object.keys(name).forEach(key => {
-      const nameKey = `name.${key}` as keyof Partial<IStudent>; // `name.firstName`
+      const nameKey = `name.${key}` as keyof Partial<IStudent>; // `name.fisrtName`
       (updatedStudentData as any)[nameKey] = name[key as keyof typeof name];
     });
   }
-
   // Dynamically handle guardian update
   if (guardian && Object.keys(guardian).length > 0) {
     Object.keys(guardian).forEach(key => {
-      const guardianKey = `guardian.${key}` as keyof Partial<IStudent>; // `guardian.fatherName`
+      const guardianKey = `guardian.${key}` as keyof Partial<IStudent>; // `guardian.fisrtguardian`
       (updatedStudentData as any)[guardianKey] =
-        guardian[key as keyof typeof guardian];
+        guardian[key as keyof typeof guardian]; // updatedStudentData['guardian.motherContactNo']=guardian[motherContactNo]
+      // updatedStudentData --> object create --> guardian : { motherContactNo: 0177}
     });
   }
-
-  // Dynamically handle localGuardian update
+  // Dynamically handle guardian update
   if (localGuardian && Object.keys(localGuardian).length > 0) {
     Object.keys(localGuardian).forEach(key => {
-      const localGuardianKey =
-        `localGuardian.${key}` as keyof Partial<IStudent>; // `localGuardian.occupation`
-      (updatedStudentData as any)[localGuardianKey] =
+      const localGuradianKey =
+        `localGuardian.${key}` as keyof Partial<IStudent>; // `localGuardian.fisrtName`
+      (updatedStudentData as any)[localGuradianKey] =
         localGuardian[key as keyof typeof localGuardian];
     });
   }
 
-  const result = await Student.findOneAndUpdate({ id }, payload, {
+  const result = await Student.findOneAndUpdate({ id }, updatedStudentData, {
     new: true,
   });
-
   return result;
 };
 
