7e6eda4a4a1e8cca5a39ea3b7c23cb36d892c5a4
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-02 01:40:34 +0600

    feat: role setup added for management department routes

--- src/app/modules/managementDepartment/managementDepartment.router.ts
+++ src/app/modules/managementDepartment/managementDepartment.router.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { ManagementDepartmentValidation } from './managementDepartment.validation';
 import { ManagementDepartmentController } from './managementDepartment.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
@@ -10,16 +12,24 @@ router.post(
   validateRequest(
     ManagementDepartmentValidation.createManagementDepartmentZodSchema
   ),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   ManagementDepartmentController.createManagementDepartment
 );
 
 router.get(
   '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
   ManagementDepartmentController.getSingleManagementDepartment
 );
 
 router.delete(
   '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN),
   ManagementDepartmentController.deleteManagementDepartment
 );
 
@@ -28,9 +38,20 @@ router.patch(
   validateRequest(
     ManagementDepartmentValidation.updateManagementDepartmentZodSchema
   ),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   ManagementDepartmentController.updateManagementDepartment
 );
 
-router.get('/', ManagementDepartmentController.getAllManagementDepartment);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  ManagementDepartmentController.getAllManagementDepartment
+);
 
 export const ManagementDepartmentRoutes = router;
