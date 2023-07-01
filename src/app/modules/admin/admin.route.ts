1b2487dddb43c36d2722498fd8b7e6fe417ebd62
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-02 01:35:15 +0600

    feat: role setup added for admin routes

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -2,18 +2,33 @@ import express from 'express';
 import { AdminController } from './admin.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AdminValidation } from './admin.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 // Routes
-router.get('/', AdminController.getAllAdmins);
-router.get('/:id', AdminController.getSingleAdmin);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AdminController.getAllAdmins
+);
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AdminController.getSingleAdmin
+);
 
-router.delete('/:id', AdminController.deleteAdmin);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN),
+  AdminController.deleteAdmin
+);
 
 router.patch(
   '/:id',
   validateRequest(AdminValidation.updateAdminZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AdminController.updateAdmin
 );
 
