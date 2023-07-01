71854ac7cf6b974a17db35a6a8c8bd1b211a4a30
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-02 01:47:14 +0600

    feat: role setup added for academic semester routes

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,21 +2,47 @@ import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
 import validateRequest from '../../middlewares/validateRequest';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 const router = express.Router();
 
 // Routes
-router.get('/', AcademicSemesterController.getAllSemesters);
-router.get('/:id', AcademicSemesterController.getSingleSemester);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AcademicSemesterController.getAllSemesters
+);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AcademicSemesterController.getSingleSemester
+);
 router.post(
   '/create-semester',
   validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.createSemester
 );
 router.patch(
   '/:id',
   validateRequest(AcademicSemesterValidation.updateAcademicSemesterZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.updateSemester
 );
-router.delete('/:id', AcademicSemesterController.deleteSemester);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteSemester
+);
 
 export const AcademicSemesterRoutes = router;
